303fb2f783a19bcf2d6c6b901c17186679137749
20211506 <20211506@gmail.com>
2023-10-19 08:51:30 +0800

    add queries in MySQL for the database

---
+++ flask.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS `web_ssis_database`;
+
+USE `web_ssis_database`;
+
+
+
+CREATE TABLE IF NOT EXISTS college (
+                'code' VARCHAR(15) NOT NULL PRIMARY KEY,
+                'name' VARCHAR(50) NOT NULL
+)
+
+
+CREATE TABLE IF NOT EXISTS Course (
+                'code' VARCHAR(20) NOT NULL PRIMARY KEY,
+                'name' VARCHAR(50) NOT NULL,
+                'college' VARCHAR(50) NOT NULL,
+                CONSTRAINT fk1 FOREIGN KEY ('college') REFERENCES college ('code') ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+
+CREATE TABLE IF NOT EXISTS Student (
+                'id' INT NOT NULL PRIMARY KEY,
+                'firstname' VARCHAR(50) NOT NULL,
+                'lastname' VARCHAR(50) NOT NULL,
+                'course'VARCHAR(30) NOT NULL,
+                'year' VARCHAR(10) NOT NULL,
+                'gender' VARCHAR(20) NOT NULL,
+                CONSTRAINT fk2 FOREIGN KEY ('course') REFERENCES course ('code') ON DELETE CASCADE ON UPDATE CASCADE
+)
