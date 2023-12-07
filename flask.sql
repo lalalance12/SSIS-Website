54121777bda48d04168afd30981f643fb6ba130a
Lance Lompon <xerxeslancelaurenz.lompon@g.msuiit.edu.ph>
2023-12-07 13:56:15 +0800

    update SQL table for improvement of data integrity

--- flask.sql
+++ flask.sql
@@ -24,5 +24,6 @@ CREATE TABLE IF NOT EXISTS Student (
                 course VARCHAR(30) NOT NULL,
                 year VARCHAR(10) NOT NULL,
                 gender VARCHAR(20) NOT NULL,
+                image_url VARCHAR(70) NOT NULL,
                 CONSTRAINT fk2 FOREIGN KEY (course) REFERENCES course (code) ON DELETE CASCADE ON UPDATE CASCADE
 )
